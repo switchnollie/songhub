c52a9be6fa68aaee0f329fadf42a3055553cd152
Tim Weise <TimWeise@hotmail.de>
2020-06-04 11:19:27 +0200

    fix missing renaming in widget

--- songhub/lib/screens/modals/add_song.dart
+++ songhub/lib/screens/modals/add_song.dart
@@ -57,7 +57,7 @@ class _AddSongFormState extends State<AddSongForm> {
   void _handleSubmit() async {
     if (_formKey.currentState.validate()) {
       String imageUrl = await _storage.uploadFile("covers", imageFile);
-      _db.add(Song(
+      _db.addSong(Song(
           title: _titleController.text,
           artist: _artistController.text,
           coverImg: imageUrl,
