bdfae8518eacb54fac69c5cece1e91e98aeda89b
Tim Weise <TimWeise@hotmail.de>
2020-06-16 22:30:58 +0200

    update cloud functions

--- songhub/functions/src/index.ts
+++ songhub/functions/src/index.ts
@@ -2,99 +2,77 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 const app = admin.initializeApp();
-// const db = app.firestore();
+const db = app.firestore();
 
-// TODO: adapt to new schema
-// export const updateArtist = functions.firestore
-//   .document("users/{userId}")
-//   .onUpdate(async (change, context) => {
-//     const newVal = change.after.data();
-//     const oldVal = change.before.data();
-//     if (newVal?.stageName === oldVal?.stageName) return null;
-//     const newStageName = newVal!.stageName;
-//     console.log(
-//       `detected rename of stageName from ${
-//         oldVal!.stageName
-//       } to ${newStageName}`
-//     );
-//     const userId = context.params.userId;
-//     const userSongsDoc = db.doc(`songs/${userId}`);
-//     const usersSongsSnapshot = await userSongsDoc.get();
-//     const usersSongs = usersSongsSnapshot.data();
-//     if (usersSongs) {
-//       let batchUpdateSongs: Promise<FirebaseFirestore.WriteResult>[] = [];
-//       for (let [songId, songObj] of Object.entries(usersSongs)) {
-//         const updatedSong = { ...songObj };
-//         updatedSong.artist = newStageName;
-//         console.log(`Updating artist field in ${songId}`);
-//         console.log({ updatedSong });
-//         batchUpdateSongs.push(userSongsDoc.update({ [songId]: updatedSong }));
-//       }
-//       return Promise.all(batchUpdateSongs);
-//     }
-//     console.log(`${newStageName} does not own any songs`);
-//     return null;
-//   });
+export const updateArtist = functions.firestore
+  .document("users/{userId}")
+  .onUpdate(async (change, context) => {
+    const newVal = change.after.data();
+    const oldVal = change.before.data();
+    if (newVal?.stageName === oldVal?.stageName) return null;
+    const newStageName = newVal.stageName;
+    console.log(
+      `detected rename of stageName from ${oldVal.stageName} to ${newStageName}`
+    );
+    const userId = context.params.userId;
+    const userSongDocuments = await db
+      .collection("users")
+      .doc(userId)
+      .collection("songs")
+      .listDocuments();
+    console.log({ userSongDocuments });
+    const songUpdateResult = await Promise.all(
+      userSongDocuments.map((songDoc) =>
+        songDoc.update({ artist: newStageName })
+      )
+    );
 
-export const testOnImageUpload = functions.storage
-  .object()
-  .onFinalize(async (object) => {
-    console.log("image uploaded to", object.bucket);
-    const storageRef = admin.storage().bucket(object.bucket);
-
-    var metadata = {
-      metadata: {
-        test: "1234",
-      },
-    };
-
-    try {
-      const setFileMetadataResponse = await storageRef
-        .file(object.name!)
-        .setMetadata(metadata);
-      console.log("Success");
-      console.log(setFileMetadataResponse[0]);
-      return null;
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
+    return songUpdateResult;
   });
 
 // manually set metadata of example data
 export const updateStorage = functions.https.onRequest(async (req, res) => {
   const storage = admin.storage(app);
+  const requests = [];
 
-  const fileQuery1 = await storage
-    .bucket("song-hub-7d14a.appspot.com")
-    .getFiles({
-      directory: "9jJgSgk2eVQF35Pa3GkvpH5RoU03/covers",
-    });
-  await fileQuery1[0][0].setMetadata({
-    metadata: {
-      owner: "9jJgSgk2eVQF35Pa3GkvpH5RoU03",
-      ypVCXwADSWSToxsRpyspWWAHNfJ2: "allowRead",
-      dMxDgggEyDTYgkcDW8O6MMOPNiD2: "allowRead",
-    },
-  });
-  const fileQuery2 = await storage
-    .bucket("song-hub-7d14a.appspot.com")
-    .getFiles({ directory: "dMxDgggEyDTYgkcDW8O6MMOPNiD2/covers" });
-  await fileQuery2[0][0].setMetadata({
-    metadata: {
-      owner: "dMxDgggEyDTYgkcDW8O6MMOPNiD2",
-      "9jJgSgk2eVQF35Pa3GkvpH5RoU03": "allowRead",
-    },
-  });
-  const fileQuery3 = await storage
-    .bucket("song-hub-7d14a.appspot.com")
-    .getFiles({
-      directory: "ypVCXwADSWSToxsRpyspWWAHNfJ2/covers",
-    });
-  await fileQuery3[0][0].setMetadata({
-    metadata: {
-      owner: "ypVCXwADSWSToxsRpyspWWAHNfJ2",
-      dMxDgggEyDTYgkcDW8O6MMOPNiD2: "allowRead",
-    },
-  });
+  // requests.push(storage
+  //   .bucket("song-hub-7d14a.appspot.com")
+  //   .file("9jJgSgk2eVQF35Pa3GkvpH5RoU03/covers/mBXkgfEmb4phulWGgTrz.jpg")
+  //   .setMetadata({
+  //     metadata: {
+  //       owner: "9jJgSgk2eVQF35Pa3GkvpH5RoU03",
+  //       ypVCXwADSWSToxsRpyspWWAHNfJ2: "allowRead",
+  //       dMxDgggEyDTYgkcDW8O6MMOPNiD2: "allowRead",
+  //     },
+  //   }));
+  requests.push(
+    storage
+      .bucket("song-hub-7d14a.appspot.com")
+      .file("dMxDgggEyDTYgkcDW8O6MMOPNiD2/covers/TdJyipldM6RrD3kzN74z.jpg")
+      .setMetadata({
+        metadata: {
+          owner: "dMxDgggEyDTYgkcDW8O6MMOPNiD2",
+          "9jJgSgk2eVQF35Pa3GkvpH5RoU03": "allowRead",
+        },
+      })
+  );
+  requests.push(
+    storage
+      .bucket("song-hub-7d14a.appspot.com")
+      .file("ypVCXwADSWSToxsRpyspWWAHNfJ2/covers/RCpg9NEGLSZ4rGRM9iHH.jpg")
+      .setMetadata({
+        metadata: {
+          owner: "ypVCXwADSWSToxsRpyspWWAHNfJ2",
+          dMxDgggEyDTYgkcDW8O6MMOPNiD2: "allowRead",
+        },
+      })
+  );
+
+  try {
+    await Promise.all(requests);
+    res.status(200).send("ok");
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Failed to set metadata");
+  }
 });
