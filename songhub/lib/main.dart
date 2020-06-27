6d5d681ca75eda6ba9d1d1517dc70c78bbb99ee6
Tim Weise <TimWeise@hotmail.de>
2020-06-27 15:58:05 +0200

    provide auth service on top level

--- songhub/lib/main.dart
+++ songhub/lib/main.dart
@@ -1,59 +1,63 @@
 import 'dart:ui';
-import 'package:song_hub/models/song.dart';
-import 'package:song_hub/models/user.dart';
+
 import 'package:song_hub/routing.dart';
 import "package:flutter/material.dart";
 import 'package:song_hub/screens/auth_guard.dart';
+import 'package:song_hub/screens/auth_widget_builder.dart';
+import 'package:song_hub/services/firebase_auth_service.dart';
+import 'package:song_hub/services/firestore_database.dart';
 import "constants.dart";
-import 'package:song_hub/services/services.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(Songhub());
+void main() => runApp(Songhub(
+      authServiceBuilder: (_) => FirebaseAuthService(),
+      databaseBuilder: (_, uid) => FirestoreDatabase(uid: uid),
+    ));
 
-class Main extends State<Songhub> {
-  final _auth = AuthService();
-  final _db = DatabaseService();
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+class Songhub extends StatelessWidget {
+  const Songhub({Key key, this.authServiceBuilder, this.databaseBuilder})
+      : super(key: key);
+  // Expose builders for 3rd party services at the root of the widget tree
+  // This is useful when mocking services while testing
+  final FirebaseAuthService Function(BuildContext context) authServiceBuilder;
+  final FirestoreDatabase Function(BuildContext context, String uid)
+      databaseBuilder;
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          StreamProvider<User>.value(value: _auth.user),
-          StreamProvider<List<Song>>.value(
-            value: _db.songs,
-            catchError: (_, err) {
-              print(err);
-              return [];
-            },
-          ),
-        ],
-        // All data will be available in this child and descendents
-        child: MaterialApp(
-          theme: ThemeData(
-            primaryColor: Color(0xFFFFFFFF),
-            accentColor: kAccentColor,
-            hintColor: kPlaceholderColor,
-            fontFamily: "Roboto",
-            textTheme: TextTheme(
-              bodyText1: TextStyle(
-                fontSize: 16.0,
-                color: kTextSecondary,
-              ),
-              headline3: TextStyle(
-                fontSize: 22.0,
-                color: kAccentColor,
-                fontWeight: FontWeight.w600,
+      providers: [
+        Provider<FirebaseAuthService>(
+          create: authServiceBuilder,
+        ),
+      ],
+      // All data will be available in this child and descendents
+      child: AuthWidgetBuilder(
+        databaseBuilder: databaseBuilder,
+        builder: (BuildContext context, AsyncSnapshot<FireUser> userSnapshot) {
+          return MaterialApp(
+            theme: ThemeData(
+              primaryColor: Color(0xFFFFFFFF),
+              accentColor: kAccentColor,
+              hintColor: kPlaceholderColor,
+              fontFamily: "Roboto",
+              textTheme: TextTheme(
+                bodyText1: TextStyle(
+                  fontSize: 16.0,
+                  color: kTextSecondary,
+                ),
+                headline3: TextStyle(
+                  fontSize: 22.0,
+                  color: kAccentColor,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
-          ),
-          home: AuthGuard(),
-          routes: routes,
-        ));
+            home: AuthGuard(),
+            routes: routes,
+          );
+        },
+      ),
+    );
   }
-}
-
-class Songhub extends StatefulWidget {
-  @override
-  Main createState() => Main();
 }
