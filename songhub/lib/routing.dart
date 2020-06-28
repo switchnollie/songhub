322d9809d64dfd267f0e2571aec765a5f67e48b6
Tim Weise <TimWeise@hotmail.de>
2020-06-28 17:01:23 +0200

    fix routes

--- songhub/lib/routing.dart
+++ songhub/lib/routing.dart
@@ -1,12 +1,14 @@
 import 'package:song_hub/models/recording.dart';
+import 'package:song_hub/screens/app/song_details/song_details_screen.dart';
+import 'package:song_hub/screens/app/songs_overview/songs_overview_screen.dart';
+import 'package:song_hub/screens/app/user_settings/user_settings_screen.dart';
 import 'package:song_hub/screens/authentication/sign_in/sign_in_screen.dart';
-import 'package:song_hub/screens/modals/song_modals.dart';
+import 'package:song_hub/screens/modals/add_song/add_song_modal.dart';
+import 'package:song_hub/screens/modals/edit_song/edit_song_modal.dart';
 import 'package:song_hub/screens/modals/recording_modals.dart';
 import 'package:song_hub/screens/app/placeholder.dart';
 import 'package:flutter/material.dart';
 import 'package:song_hub/screens/authentication/sign_up/sign_up_screen.dart';
-import 'package:song_hub/screens/modals/user_settings_modal.dart';
-import 'package:song_hub/viewModels/recording_with_images.dart';
 import 'package:song_hub/viewModels/song_with_images.dart';
 
 class SongDetailsScreenRouteParams {
@@ -26,15 +28,14 @@ class RecordingModalRouteParams {
 }
 
 Map<String, Widget Function(BuildContext)> routes = {
-  SongOverviewScreen.routeId: (context) => SongOverviewScreen(),
-  SongDetailsScreen.routeId: (context) => SongDetailsScreen(),
-  AddSongModal.routeId: (context) => AddSongModal(),
-  EditSongModal.routeId: (context) => EditSongModal(),
-  AddRecordingModal.routeId: (context) => AddRecordingModal(),
-  EditRecordingModal.routeId: (context) => EditRecordingModal(),
-  "/notifications": (context) => PlaceholderScreen("Notifications"),
-  ProfileScreen.routeId: (context) => ProfileScreen(),
-  SignInScreen.routeId: (context) => SignInScreenBuilder(),
+  SongsOverviewScreen.routeId: SongsOverviewScreen.create,
+  SongDetailsScreen.routeId: SongDetailsScreen.create,
+  AddSongModal.routeId: (_) => AddSongModal(),
+  EditSongModal.routeId: (_) => EditSongModal(),
+  AddRecordingModal.routeId: (_) => AddRecordingModal(),
+  EditRecordingModal.routeId: (_) => EditRecordingModal(),
+  "/notifications": (_) => PlaceholderScreen("Notifications"),
+  UserSettingsScreen.routeId: (_) => UserSettingsScreen(),
+  SignInScreen.routeId: (_) => SignInScreenBuilder(),
   SignUpScreen.routeId: (context) => SignUpScreenBuilder(),
-  UserSettingsModal.routeId: (context) => UserSettingsModal(),
 };
