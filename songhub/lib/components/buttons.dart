f813dfac107c6f3645ef60d1775675ccad0cc65b
Pascal Schlaak <schlaak.pascal@gmail.com>
2020-07-06 15:42:50 +0200

    Added top padding to primary button

--- songhub/lib/components/buttons.dart
+++ songhub/lib/components/buttons.dart
@@ -19,26 +19,29 @@ class PrimaryButton extends StatelessWidget {
   PrimaryButton({@required this.text, @required this.onPressed});
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: SizedBox(
-        width: double.infinity,
-        height: 54,
-        child: RaisedButton(
-          textColor: Colors.white,
-          elevation: 0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Text(
-              text.toUpperCase(),
-              style: TextStyle(
-                fontSize: 16.0,
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: InkWell(
+        child: SizedBox(
+          width: double.infinity,
+          height: 54,
+          child: RaisedButton(
+            textColor: Colors.white,
+            elevation: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                text.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
             ),
+            color: Theme.of(context).accentColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(8.0)),
+            onPressed: onPressed,
           ),
-          color: Theme.of(context).accentColor,
-          shape: RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(8.0)),
-          onPressed: onPressed,
         ),
       ),
     );
